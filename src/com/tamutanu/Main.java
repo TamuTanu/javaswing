4336301d72ef62a1d43fd26a250f637c2119beb2
TamuTanu <tamutanu.contact@gmail.com>
2022-03-24 01:01:11 +0700

    Initial commit

---
+++ src/com/tamutanu/Main.java
@@ -0,0 +1,19 @@
+package com.tamutanu;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    	// Layout Manager = Defines the natural layout for components within a container
+    	//FlowLayout = places components in a row,sized at their preferred size.
+    	//			   If the horizontal space in the container is too small,
+    	//			   the Flowlayout class uses the next avalible now.
+    	
+    }
+}
